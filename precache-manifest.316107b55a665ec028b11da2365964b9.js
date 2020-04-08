52ab5fa74bb281c4d53e04c70489da05d927d744
user <abhay_pratap@thbs.com>
2020-04-08 14:46:40 +0100

    Updates

---
+++ precache-manifest.316107b55a665ec028b11da2365964b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0564371292b8f4148b4650702116234",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "e8bc29417443399b5e33",
+    "url": "/covid-19/static/css/main.31cb038f.chunk.css"
+  },
+  {
+    "revision": "08783ae6aa15826433c5",
+    "url": "/covid-19/static/js/2.48a4e4c2.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/covid-19/static/js/2.48a4e4c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8bc29417443399b5e33",
+    "url": "/covid-19/static/js/main.1883bafa.chunk.js"
+  },
+  {
+    "revision": "e84dd164dd3b06d1310b",
+    "url": "/covid-19/static/js/runtime-main.6ca52c11.js"
+  },
+  {
+    "revision": "65fe8e5b75e58b06e88480ade292ad28",
+    "url": "/covid-19/static/media/rotatedTitle.65fe8e5b.png"
+  },
+  {
+    "revision": "808129791a400bfad4c47341347c9727",
+    "url": "/covid-19/static/media/tree.80812979.jpg"
+  },
+  {
+    "revision": "bde89f9089f5fa7762331e4b4a924f5d",
+    "url": "/covid-19/static/media/world.bde89f90.jpg"
+  }
+]);
